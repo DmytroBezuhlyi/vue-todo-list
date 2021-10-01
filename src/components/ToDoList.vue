0a7e0a81fa43bb9f8f76b6c2aa0431de4aafe79d
Dmytro <bezugly.dmitry@gmail.com>
2021-10-01 20:40:39 +0300

    fixed animation on enter/leave first todo

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
     <h2>TODOS' List</h2>
-    <div v-if="todoList.length && !isLoading" class="list">
+    <transition name="fade">
+      <div class="empty" v-show="!todoList.length && !isLoading">
+        <h4>There aren't any todos in your list</h4>
+      </div>
+    </transition>
+    <div class="list">
       <transition-group name="list" tag="p">
-      <ToDoItem
-          v-for="todo in todoList"
-          :key="todo.id"
-          :todo="todo"
-          @remove="$emit('remove', todo)"
-      />
+        <ToDoItem
+            v-for="todo in todoList"
+            :key="todo.id"
+            :todo="todo"
+            @remove="$emit('remove', todo)"
+        />
       </transition-group>
-    </div>
-    <div class="empty" v-else-if="!todoList.length && !isLoading">
-      <h4>There aren't any todos in your list</h4>
     </div>
   </div>
 </template>
@@ -40,7 +42,7 @@ export default {
 .empty h4 {
   font-weight: 600;
   color: navy;
-  text-shadow: 1px 1px 5px rgba(0,0,0,0.5);
+  text-shadow: 1px 1px 5px rgba(0, 0, 0, 0.5);
   text-align: center;
 }
 
@@ -50,11 +52,29 @@ export default {
   flex-wrap: wrap;
   margin: 0.5rem 0 0;
 }
-.list-enter-active, .list-leave-active {
-  transition: all 1s;
+
+.list-enter-active {
+  transition: all 1s .5s;
 }
-.list-enter, .list-leave-to /* .list-leave-active до версии 2.1.8 */ {
+
+.list-leave-active {
+  transition: all .5s;
+}
+
+.list-enter, .list-leave-to /* .list-leave-active до версии 2.1.8 */
+{
   opacity: 0;
   transform: translateY(30px);
+}
+
+.fade-enter-active {
+  transition: opacity .5s 1s;
+}
+
+.fade-leave-active {
+  transition: opacity .3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */ {
+  opacity: 0;
 }
 </style>
