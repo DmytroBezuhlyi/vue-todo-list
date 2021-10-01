c05bf74371509ae1531484113512327675cdc1cb
Dmytro <bezugly.dmitry@gmail.com>
2021-10-02 01:50:45 +0300

    added dynamic routes for single todo

--- src/store/modules/todosModule.js
+++ src/store/modules/todosModule.js
@@ -1,11 +1,23 @@
 export const todosModule = {
   state: () => ({
     todoList: [],
+    todo: [],
     isLoading: false
   }),
+  getters: {
+    getTodoList(state) {
+      return state.todoList;
+    },
+    getTodo(state) {
+      return state.todo;
+    }
+  },
   mutations: {
     setTodoList(state, list) {
       state.todoList = list;
+    },
+    setTodo(state, todo) {
+      state.todo = todo;
     },
     setIsLoading(state, load) {
       state.isLoading = load;
@@ -17,11 +29,11 @@ export const todosModule = {
       try {
         setTimeout(() => {
           if (localStorage.getItem('list') && localStorage.getItem('list') !== 'undefined') {
-          const list = JSON.parse(localStorage.getItem('list'))
-          commit("setTodoList", list);
-          commit("setIsLoading", false);
-        }
-      }, 1000);
+            const list = JSON.parse(localStorage.getItem('list'))
+            commit("setTodoList", list);
+            commit("setIsLoading", false);
+          }
+        }, 1000);
       } catch (e) {
         console.error(e);
       } finally {
@@ -32,6 +44,10 @@ export const todosModule = {
     },
     updateLocalStorage({state}) {
       localStorage.setItem('list', JSON.stringify(state.todoList));
+    },
+    fetchTodo({state, commit}, todoId) {
+      const todo = state.todoList.filter(t => t.id === +todoId);
+      commit('setTodo', todo);
     }
   },
   namespaced: true
