df243cb3d92b9473fbf4f54c97288788dda2e71d
Dmytro Bezuhlyi | CHISW JS Dev <dmytro.bezuhlyi@chisw,com>
2021-10-06 12:46:49 +0300

    refactored store organization

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,50 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import {todosModule} from "@/store/modules/todosModule"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules: {
-    todosModule: todosModule
+  state: () => ({
+    todoList: [],
+    isLoading: false
+  }),
+  getters: {
+    getTodoList: (state) => {
+      return state.todoList
+    },
+    getTodoById: (state) => (id) => {
+      return state.todoList.find(td => td.id === id);
+    }
+  },
+  mutations: {
+    setTodoList(state, list) {
+      state.todoList = list;
+    },
+    setIsLoading(state, load) {
+      state.isLoading = load;
+    }
+  },
+  actions: {
+    fetchList({commit}) {
+      commit("setIsLoading", true);
+      try {
+        setTimeout(() => {
+          if (localStorage.getItem('list') && localStorage.getItem('list') !== 'undefined') {
+            const list = JSON.parse(localStorage.getItem('list'));
+            commit("setTodoList", list);
+            commit("setIsLoading", false);
+          }
+        }, 1000);
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setTimeout(() => {
+          commit("setIsLoading", false);
+        }, 1500);
+      }
+    },
+    updateLocalStorage({state}) {
+      localStorage.setItem('list', JSON.stringify(state.todoList));
+    }
   }
 })
