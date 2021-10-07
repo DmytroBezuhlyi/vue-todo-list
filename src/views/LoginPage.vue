26b9170ac7c5261d4ae2665a1be72bba99a009a1
Dmytro Bezuhlyi | CHISW JS Dev <dmytro.bezuhlyi@chisw,com>
2021-10-07 12:33:59 +0300

    added preloaders

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
-    <LoginForm/>
+    <div v-if="isLoading">
+      <ToDoPreloader/>
+    </div>
+    <div v-else>
+      <LoginForm/>
+    </div>
   </div>
 </template>
 
 <script>
-import LoginForm from "../components/LoginForm";
+import LoginForm from "@/components/LoginForm";
+import preloaderMixin from "@/mixins/preloaderMixin";
+import ToDoPreloader from "../components/UI/ToDoPreloader";
 
 export default {
   name: "LoginPage",
-  components: {LoginForm}
+  components: {LoginForm, ToDoPreloader},
+  mixins: [preloaderMixin],
 }
 </script>
 
--- src/views/RegistrationPage.vue
+++ src/views/RegistrationPage.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
-    <RegistrationForm/>
+    <div v-if="isLoading">
+      <ToDoPreloader/>
+    </div>
+    <div v-else>
+      <RegistrationForm/>
+    </div>
   </div>
 </template>
 
 <script>
 import RegistrationForm from "@/components/RegistartionForm";
+import ToDoPreloader from "@/components/UI/ToDoPreloader";
+import preloaderMixin from "../mixins/preloaderMixin";
+
 export default {
   name: "RegistrationPage",
-  components: {RegistrationForm}
+  components: {RegistrationForm, ToDoPreloader},
+  mixins: [preloaderMixin],
 }
 </script>
 
